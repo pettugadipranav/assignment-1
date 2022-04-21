db8975f98d3302e6f74001fe4f902dac591315cf
pettugadipranav <102520050+pettugadipranav@users.noreply.github.com>
2022-04-21 22:00:57 +0530

    Add files via upload

---
+++ code/assignment1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+/* let the age of  vivek be 'a' and the age of younger brother amit be 'b' */
+int main(){
+int a=25;
+int b=22;
+printf("sum of their ages=%d\n",a+b);
+printf("product of their ages=%d",a*b);
+
+}
